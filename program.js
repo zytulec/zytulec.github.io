03df4eb196f637108cd249a4af4daa1b6df34332
zytulec <147132470+zytulec@users.noreply.github.com>
2025-09-08 14:09:29 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,1 +1,16 @@
+const dodaj = () => {
+   const f1 = document.getElementById('f1')
+   const f2 = document.getElementById('f2')
+
+   const wynik = document.getElementById('wynik')
+
+   wynik.innerText = Number(f1.value) + Number(f2.value)
+}
+
+const wyczysc = () =>{
+
+   f1.value = ''
+   f2.value = ''
+   wynik.innerText = ''
+}
 
